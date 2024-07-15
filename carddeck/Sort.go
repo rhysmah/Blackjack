5f179e71e7f6cb4bf18fcbe96d528841e64a0e1e
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-07-15 11:09:32 -0700

    chore: add docs to `Sort`

--- carddeck/Sort.go
+++ carddeck/Sort.go
@@ -2,22 +2,34 @@ package carddeck
 
 import "sort"
 
+// BySuit is a type that implements the sort.Interface for a slice of Card objects, sorting by suit.
 type BySuit []Card
 
 func (a BySuit) Len() int           { return len(a) }
 func (a BySuit) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a BySuit) Less(i, j int) bool { return a[i].Suit < a[j].Suit }
 
+// SortBySuit sorts a slice of Card objects by suit.
+//
+// Parameters:
+//
+//	deckOfCards []Card - A slice of Card objects to be sorted.
 func SortBySuit(deckOfCards []Card) {
 	sort.Sort(BySuit(deckOfCards))
 }
 
+// ByValue is a type that implements the sort.Interface for a slice of Card objects, sorting by value.
 type ByValue []Card
 
 func (a ByValue) Len() int           { return len(a) }
 func (a ByValue) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByValue) Less(i, j int) bool { return a[i].Value < a[j].Value }
 
+// SortByValue sorts a slice of Card objects by value.
+//
+// Parameters:
+//
+//	deckOfCards []Card - A slice of Card objects to be sorted.
 func SortByValue(deckOfCards []Card) {
 	sort.Sort(ByValue(deckOfCards))
 }
